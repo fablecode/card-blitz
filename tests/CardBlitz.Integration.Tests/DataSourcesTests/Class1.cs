e0969bb97409ab8aa3ee902797fac2f666d87cac
fablecode <fablecode@users.noreply.github.com>
2024-09-07 12:05:32 +0200

    Integration test project added.

---
+++ tests/CardBlitz.Integration.Tests/DataSourcesTests/Class1.cs
@@ -0,0 +1,37 @@
+﻿using CardBlitz.DataSources;
+using CardBlitz.Models;
+using NUnit.Framework;
+
+namespace CardBlitz.Integration.Tests.DataSourcesTests
+{
+    [TestFixture]
+    public class CardByNameTests
+    {
+        [SetUp]
+        public void SetUp()
+        {
+            var datasource = new WikiDataSource();
+        }
+
+        [TestCase("Odd-Eyes Pendulumgraph Dragon")]
+        public void Given_A_Card_Name_Should_Return_Card_Name_From_Data_Source(string cardName)
+        {
+            // Arrange
+            // Act
+            // Assert
+        }
+    }
+
+    public class WikiDataSource : ICardDataSource
+    {
+        public Card CardByName(string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Card CardByPasscode(long passcode)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
