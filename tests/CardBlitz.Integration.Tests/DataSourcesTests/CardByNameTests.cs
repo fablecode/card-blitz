d98e805139c698c78fd5c0d6547972d7ebd3d985
fablecode <fablecode@users.noreply.github.com>
2024-09-09 13:05:04 +0200

    Card by id integration test extracted.

--- tests/CardBlitz.Integration.Tests/DataSourcesTests/CardByNameTests.cs
+++ tests/CardBlitz.Integration.Tests/DataSourcesTests/CardByNameTests.cs
@@ -16,18 +16,6 @@ namespace CardBlitz.Integration.Tests.DataSourcesTests
             _cardDataSouce = new WikiDataSource(new CardWebPage(new CardHtmlDocument(new CardHtmlTable()), new HtmlWebPage()));
         }
 
-        [TestCase("Odd-Eyes Pendulumgraph Dragon", 66425726)]
-        public void Given_A_Card_Name_Should_Return_Card_Id_From_Data_Source(string cardName, long expected)
-        {
-            // Arrange
-
-            // Act
-            var result = _cardDataSouce.CardByName(cardName);
-
-            // Assert
-            result.Id.Should().Be(expected);
-        }
-
         [TestCase("Odd-Eyes Pendulumgraph Dragon", "Odd-Eyes Pendulumgraph Dragon")]
         public void Given_A_Card_Name_Should_Return_Card_Name_From_Data_Source(string cardName, string expected)
         {
