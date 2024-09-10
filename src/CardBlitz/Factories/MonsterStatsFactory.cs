055eca2b2584273b05b0fb96074718168d99d3b6
fablecode <fablecode@users.noreply.github.com>
2024-09-10 21:36:38 +0200

    MonsterStatsFactory tests

--- src/CardBlitz/Factories/MonsterStatsFactory.cs
+++ src/CardBlitz/Factories/MonsterStatsFactory.cs
@@ -11,7 +11,34 @@ public class MonsterStatsFactory
         var stats = yugiohWebPageCard.AtkDef.Split('/');
 
         return !yugiohWebPageCard.MonsterLinkArrows.Any()
-            ? new DefensibleStat(Convert.ToInt32(stats.First()), Convert.ToInt32(stats.Last()))
-            : new LinkStat(Convert.ToInt32(stats.First()), Convert.ToInt32(stats.Last()));
+            ? new DefensibleStat(ConvertAtkDef(stats.First()), ConvertAtkDef(stats.Last()))
+            : new LinkStat(ConvertAtkLinkRating(stats.First()), ConvertAtkLinkRating(stats.Last()));
+    }
+
+    public static int ConvertAtkLinkRating(string atkLinkRating)
+    {
+        return ConvertStats(atkLinkRating,"Invalid ATK/LinkRating value.");
+    }
+
+    public static int ConvertAtkDef(string atkDef)
+    {
+        return  ConvertStats(atkDef, "Invalid ATK/DEF value.");
+    }
+    public static int ConvertStats(string monsterStat, string errorMessage)
+    {
+        // Check if the value is a question mark, indicating an undefined or variable value
+        if (monsterStat == "?")
+        {
+            return 0;  // Return the default value (e.g., 0)
+        }
+
+        // Otherwise, attempt to parse the string as an integer
+        if (int.TryParse(monsterStat, out var result))
+        {
+            return result;
+        }
+
+        // If parsing fails, throw an exception or handle the error as needed
+        throw new ArgumentException(errorMessage, nameof(monsterStat));
     }
 }
---
+++ tests/CardBlitz.Unit.Tests/FactoriesTests/MonsterStatsFactoryTests/CreateMonsterStatsTests.cs
@@ -0,0 +1,100 @@
+﻿using CardBlitz.Domain;
+using CardBlitz.Domain.Monster;
+using CardBlitz.Factories;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace CardBlitz.Unit.Tests.FactoriesTests.MonsterStatsFactoryTests;
+
+[TestFixture]
+public class CreateMonsterStatsTests
+{
+    [Test]
+    public void Given_A_YugiohWebPageCard_If_Card_Is_Standard_Return_DefensibleStat_Type()
+    {
+        // Arrange
+        var yugiohWebPageCard = new YugiohWebPageCard();
+        yugiohWebPageCard.AtkDef = "3000 / 2500";
+
+        // Act
+        var result = MonsterStatsFactory.CreateMonsterStats(yugiohWebPageCard);
+
+        // Assert
+        result.Should().BeOfType<DefensibleStat>();
+    }
+    [Test]
+    public void Given_A_YugiohWebPageCard_If_Card_Is_Standard_Return_LinkStat_Type()
+    {
+        // Arrange
+        var yugiohWebPageCard = new YugiohWebPageCard();
+        yugiohWebPageCard.AtkDef = "3000 / 4";
+        yugiohWebPageCard.LinkArrows = "Top LM-Top, Bottom-Left LM-BottomLeft, Bottom LM-Bottom, Bottom-Right LM-BottomRight";
+
+        // Act
+        var result = MonsterStatsFactory.CreateMonsterStats(yugiohWebPageCard);
+
+        // Assert
+        result.Should().BeOfType<LinkStat>();
+    }
+}
+
+[TestFixture]
+public class ConvertAtkDefRatingTests
+{
+    [TestCase("3000", 3000)]
+    [TestCase("2500", 2500)]
+    [TestCase("?", 0)]
+    public void Given_A_Monster_Attack_Or_Defense_Should_Convert_Attack_Or_Defense_To_An_Integer_Value(string atkDef, int expected)
+    {
+        // Arrange
+        // Act
+        var result = MonsterStatsFactory.ConvertAtkDef(atkDef);
+
+        // Assert
+        result.Should().Be(expected);
+    }
+
+    [Test]
+    public void Given_An_Invalid_Monster_Attack_Or_Defense_Should_Throw_ArgumentException()
+    {
+        // Arrange
+        const string atkDef = "invalid atk or defence";
+
+        // Act
+        Action act = () => MonsterStatsFactory.ConvertAtkDef(atkDef);
+
+        // Assert
+        act.Should().Throw<ArgumentException>();
+    }
+}
+
+[TestFixture]
+public class ConvertAtkLinkRatingTests
+{
+    [TestCase("3000", 3000)]
+    [TestCase("4", 4)]
+    [TestCase("?", 0)]
+    public void Given_A_Monster_Attack_Or_LinkRating_Should_Convert_Attack_Or_LinkRating_To_Integer_Value(string atkLinkRating, int expected)
+    {
+        // Arrange
+        // Act
+        var result = MonsterStatsFactory.ConvertAtkLinkRating(atkLinkRating);
+
+        // Assert
+        result.Should().Be(expected);
+    }
+
+    [Test]
+    public void Given_An_Invalid_Monster_Attack_Or_LinkRating_Should_Throw_ArgumentException()
+    {
+        // Arrange
+        const string atkDef = "invalid atk or link rating";
+
+        // Act
+        Action act = () => MonsterStatsFactory.ConvertAtkLinkRating(atkDef);
+
+        // Assert
+        act.Should().Throw<ArgumentException>();
+    }
+
+}
